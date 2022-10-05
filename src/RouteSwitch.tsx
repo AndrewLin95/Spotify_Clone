41bea69a4a1c4b58f4bc1a42eed1d23175f4f9f8
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-04 21:10:15 -0400

    removed authprovider code

--- src/RouteSwitch.tsx
+++ src/RouteSwitch.tsx
@@ -1,6 +1,4 @@
-import { BrowserRouter, Routes, Route, useNavigate } from "react-router-dom";
-import * as React from 'react';
-import { fakeAuthProvider } from './Components/Util/fakeAuthProvider';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header/Header";
 import Footer from "./Components/Footer/Footer";
 import Login from "./Components/Login/Login";
@@ -8,74 +6,15 @@ import App from "./App";
 
 const RouteSwitch = () => {
   return (
-    <AuthProvider>
-      <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path="" element={<Login />} />
-            <Route path="/home" element={<App />}/>
-          </Routes>
-          <Footer />
-      </BrowserRouter>
-    </AuthProvider>
+    <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="" element={<Login />} />
+          <Route path="/home" element={<App />}/>
+        </Routes>
+        <Footer />
+    </BrowserRouter>
   )
 }
-
-interface AuthContextType {
-  user: any;
-  signin: (user: string, callback: VoidFunction) => void;
-  signout: (callback: VoidFunction) => void;
-}
-
-let AuthContext = React.createContext<AuthContextType>(null!);
-
-function AuthProvider({ children }: { children: React.ReactNode }) {
-  let [user, setUser] = React.useState<any>(null);
-
-  let signin = (newUser: string, callback: VoidFunction) => {
-    return fakeAuthProvider.signin(() => {
-      setUser(newUser);
-      callback();
-    });
-  };
-
-  let signout = (callback: VoidFunction) => {
-    return fakeAuthProvider.signout(() => {
-      setUser(null);
-      callback();
-    });
-  };
-
-  let value = { user, signin, signout };
-
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
-}
-
-function useAuth() {
-  return React.useContext(AuthContext);
-}
-
-function AuthStatus() {
-  let auth = useAuth();
-  let navigate = useNavigate();
-
-  if (!auth.user) {
-    return <p>You are not logged in.</p>;
-  }
-
-  return (
-    <p>
-      Welcome {auth.user}!{" "}
-      <button
-        onClick={() => {
-          auth.signout(() => navigate("/"));
-        }}
-      >
-        Sign out
-      </button>
-    </p>
-  );
-}
-
 
 export default RouteSwitch;
