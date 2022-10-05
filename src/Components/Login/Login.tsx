b90638a62cc576bf2d5bc1abd3a7e7ef5dbdc61a
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-04 21:27:07 -0400

    added switch statement to display the login or access screen based on authentication status from Spotify using the url token redirect

--- src/Components/Login/Login.tsx
+++ src/Components/Login/Login.tsx
@@ -1,9 +1,11 @@
-import { FC, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
+import { Link } from "react-router-dom";
 import './style.css';
 import { getTokenFromUrl, loginUrl } from '../Spotify/Spotify';
 
-const Login: FC = () => {
-  const [token, setToken] = useState();
+
+const Login = () => {
+  const [token, setToken] = useState<undefined | string>();
 
   useEffect(() => {
     const hash = getTokenFromUrl();
@@ -13,20 +15,38 @@ const Login: FC = () => {
     if (_token) {
       setToken(_token);
     }
-
-    console.log("token", token);
   }, []);
 
-  return (
-    <div id='loginContainer'>
-      <img 
-        id="spotifyLoginLogo"
-        src={`${process.env.PUBLIC_URL}/assets/Spotify_Logo_RGB_Green.png`} 
-        alt="spotifylogo"
-      />
-      <a href={loginUrl}>Login with Spotify</a>
-    </div>
-  )
+  useEffect(() => {
+    console.log("token", typeof(token))
+  }, [token])
+
+  switch (typeof(token)) {
+    case "undefined":
+      return (
+        <div id='loginContainer'>
+          <img 
+            id="spotifyLoginLogo"
+            src={`${process.env.PUBLIC_URL}/assets/Spotify_Logo_RGB_Green.png`} 
+            alt="spotifylogo"
+          />
+          <a href={loginUrl}>Login with Spotify</a>
+        </div>
+      );
+
+      // TODO: create a more intuitive access screen
+    case "string":
+      return (
+        <div id='loginContainer'>
+          <img 
+            id="spotifyLoginLogo"
+            src={`${process.env.PUBLIC_URL}/assets/Spotify_Logo_RGB_Green.png`} 
+            alt="spotifylogo"
+          />
+          <Link className="naveLinks" to="/home">Click here to Access Spotify</Link>
+        </div>
+      )
+  }
 }
 
 export default Login;
