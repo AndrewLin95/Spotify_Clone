6f8753a7d2c4703168a40ec0d1fc09f3f5334a75
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-10 18:48:00 -0400

    wrote query function to retrieve recommended artists from Spotify's API based on the top five user artists

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -1,21 +1,18 @@
 import { FC } from 'react';
 import HomePlaylist from './HomePlaylist/HomePlaylist';
 import HomeTopArtist from './HomeTopArtist/HomeTopArtist';
-import HomeRecentlyPlayedTrack from './HomeRecentlyPlayedTrack/HomeRecentlyPlayedTrack';
 import './style.css';
 
 interface Props{
   userPlaylist: SpotifyApi.PlaylistObjectSimplified[],
   userTopArtists: SpotifyApi.ArtistObjectFull[],
-  userRecentlyPlayedTrack: SpotifyApi.PlayHistoryObject[]
 }
 
-const Home: FC<Props> = ({ userPlaylist, userTopArtists, userRecentlyPlayedTrack }) => {
+const Home: FC<Props> = ({ userPlaylist, userTopArtists }) => {
   return (
     <div id='homeContainer'>
       <HomePlaylist userPlaylist={userPlaylist}/>
       <HomeTopArtist userTopArtists={userTopArtists} />
-      <HomeRecentlyPlayedTrack userRecentlyPlayedTrack={userRecentlyPlayedTrack} />
     </div>
   )
 }
