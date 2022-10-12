38d84e1e732a808fd58ceec516821c5e05acb26f
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-11 20:24:31 -0400

    added Fast Average Color and styled music Header

--- src/Components/Music/MusicHeader/MusicHeader.tsx
+++ src/Components/Music/MusicHeader/MusicHeader.tsx
@@ -1,14 +1,30 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 import './style.css';
+import { FastAverageColor } from 'fast-average-color';
 
 interface Props{
   currPlaylistAlbum: SpotifyApi.PlaylistObjectSimplified
 }
 
 const MusicHeader:FC<Props> = ({ currPlaylistAlbum }) => {
+  const [domColor, setDomColor] = useState<any>({value: [ 20, 20, 20 ]});
+  
+  const fac = new FastAverageColor();
+
+  useEffect(() => {
+    const image: any = document.querySelector('.musicHeaderImage');
+    // color = fac.getColor(image);
+    fac.getColorAsync(image, { algorithm: 'dominant'})
+      .then(color => {
+        setDomColor(color);
+      }).catch(e => {
+        throw(e);
+      })
+  }, [])
+
   return (
-    <div className='musicHeaderMain'>
-      <img className='musicHeaderImage' src={currPlaylistAlbum.images[0].url} />
+    <div className='musicHeaderMain' style={{backgroundColor: `rgba(${domColor.value[0]}, ${domColor.value[1]}, ${domColor.value[2]}, 0.7)`}}>
+      <img className='musicHeaderImage' src={currPlaylistAlbum.images[0].url} crossOrigin='anonymous' width={`200px`} height={`200px`}/>
       <div className='musicHeaderText'>
         <div className='musicHeaderType'>{currPlaylistAlbum.type.toUpperCase()}</div>
         <div className='musicHeaderName'>{currPlaylistAlbum.name}</div>
