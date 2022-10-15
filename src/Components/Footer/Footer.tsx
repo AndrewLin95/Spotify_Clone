8b9a66d7f075ce4b12bd2eec2e9e7b5b20f22936
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-15 14:04:19 -0400

    added component for the time slider to display track time

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,13 +1,23 @@
+import { useEffect, useState } from 'react';
 import './style.css';
+import TimeSlider from './TimeSlider/TimeSlider';
 
 const Footer = () => {
+
   return (
     <div id='footerContainer'>
       <div id='footerLeft'>
         
       </div>
       <div id='footerCenter'>
+        <div id='footerPlaybackControlsContainer'>
 
+        </div>
+        <div id='footerTimeSliderContainer'>
+          <div className='footerCurrTime'></div>
+          <div className='footerTimeSlider'><TimeSlider /></div>
+          <div className='footerTotalTime'></div>
+        </div>
       </div>
       <div id='footerRight'>
 
---
+++ src/Components/Footer/TimeSlider/TimeSlider.tsx
@@ -0,0 +1,19 @@
+import { FC, useState } from 'react';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import Slider from '@mui/material/Slider';
+
+const TimeSlider = () => {
+
+  const [timeValue, setTimeValue] = useState(0);
+
+  return (
+    <Box sx={{width: 200}}>
+      <Stack spacing={2} direction='row' alignItems='center'>
+        <Slider aria-label='TrackTime' value={timeValue} />
+      </Stack>
+    </Box>
+  )
+}
+
+export default TimeSlider;
