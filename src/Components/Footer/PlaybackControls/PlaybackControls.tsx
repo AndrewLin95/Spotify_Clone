bd803a1826a377ce1c0807b4129acf8f1570ea41
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-18 16:57:49 -0400

    added shuffle and repeat icons

--- src/Components/Footer/PlaybackControls/PlaybackControls.tsx
+++ src/Components/Footer/PlaybackControls/PlaybackControls.tsx
@@ -5,6 +5,11 @@ import PauseRounded from '@mui/icons-material/PauseRounded';
 import PlayArrowRounded from '@mui/icons-material/PlayArrowRounded';
 import FastForwardRounded from '@mui/icons-material/FastForwardRounded';
 import FastRewindRounded from '@mui/icons-material/FastRewindRounded';
+import Shuffle from '@mui/icons-material/Shuffle';
+import ShuffleOn from '@mui/icons-material/ShuffleOn';
+import Repeat from '@mui/icons-material/Repeat';
+import RepeatOn from '@mui/icons-material/RepeatOn';
+import RepeatOne from '@mui/icons-material/RepeatOne';
 
 interface Props{
   player: any,
@@ -15,12 +20,13 @@ const PlaybackControls:FC<Props> = ({ player, is_paused }) => {
   return(
     <Box sx={{width: 300, display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
       <Stack spacing={1} direction='row' alignItems='center'>
+        <div> <Shuffle fontSize='small' /> </div>
         <div onClick={() => {player.previousTrack()}}><FastRewindRounded /></div>
         <div onClick={() => {player.togglePlay()}}>
-          {is_paused ? <PlayArrowRounded /> : <PauseRounded /> }
+          {is_paused ? <PlayArrowRounded fontSize='large' /> : <PauseRounded fontSize='large'/> }
         </div>
         <div onClick={() => {player.nextTrack()}}><FastForwardRounded /></div>
-        
+        <div> <Repeat fontSize='small'/> </div>
       </Stack>
     </Box>
   )
