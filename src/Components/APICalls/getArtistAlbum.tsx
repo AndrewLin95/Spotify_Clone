1f57d7a4bf167dff9bb00c6de06e3f0a9e1bd6aa
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-22 12:03:51 -0400

    added data manipulation to artists albums to return albums, singles and other albums

--- src/Components/APICalls/getArtistAlbum.tsx
+++ src/Components/APICalls/getArtistAlbum.tsx
@@ -1,3 +1,5 @@
+import { parseISO } from "date-fns/esm";
+import isAfter from "date-fns/isAfter";
 
 export default async function getArtistAlbum(artistID: string, token:string ) {
   
@@ -20,7 +22,41 @@ export default async function getArtistAlbum(artistID: string, token:string ) {
     const response = await fetch(url, requestOptions);
     const data = await response.json();
 
-    return data;
+    // sorts the response by date. Earliest to latest
+    const sortedData = data.items.sort((a: any, b: any) => {
+      const aDate = a.release_date;
+      const bDate = b.release_date;
+
+      if (isAfter(parseISO(aDate), parseISO(bDate))){
+        return -1;
+      } else {
+        return 1;
+      }
+    });
+
+    // returns albums only, sorted
+    const dataAlbum = sortedData.reduce((albums:any, dataInput: any) => {
+      if (dataInput.album_group === 'album'){
+        return [...albums, dataInput]
+      }
+      return [...albums];
+    }, [])
+    // returns singles only
+    const dataSingles = sortedData.reduce((singles:any, dataInput: any) => {
+      if (dataInput.album_group === 'single'){
+        return [...singles, dataInput]
+      }
+      return [...singles];
+    }, [])
+    // returns all other data
+    const dataOther = sortedData.reduce((other:any, dataInput: any) => {
+      if (dataInput.album_group === 'appears_on'){
+        return [...other, dataInput]
+      }
+      return [...other];
+    }, [])
+
+    return {sortedData, dataAlbum, dataSingles, dataOther};
   } catch (err) {
     throw err;
   }
