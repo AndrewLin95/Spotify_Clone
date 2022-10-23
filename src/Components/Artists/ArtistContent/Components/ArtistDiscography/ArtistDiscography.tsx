edf28b147b81f80f2c27c077c59ef7a60a7a095e
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-23 14:23:35 -0400

    created a toggle button for album states switch

--- src/Components/Artists/ArtistContent/Components/ArtistDiscography/ArtistDiscography.tsx
+++ src/Components/Artists/ArtistContent/Components/ArtistDiscography/ArtistDiscography.tsx
@@ -1,15 +1,47 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import Stack from '@mui/material/Stack';
+import ToggleButton from '@mui/material/ToggleButton';
+import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import { ArtistAlbum } from '../../../../Util/modals';
 
 interface Props{
   artistAlbums: ArtistAlbum,
 }
 
+const albumStates = {
+  All: 'All',
+  Albums: 'Albums',
+  Singles: 'Singles',
+  Others: 'Others',
+}
+
 const ArtistDiscography:FC<Props> = ({ artistAlbums }) => {
+  const [albumState, setAlbumState] = useState(albumStates.All);
+
+  // updates the toggle button for soup choices.
+  const handleAlbumChange = (
+    event: React.MouseEvent<HTMLElement>,
+    _albumState: string
+  ) => {
+    // if _albumState !== null is added, it enforces that one button is selected at all times
+    if (_albumState !== null) {
+      setAlbumState(_albumState);
+    }
+  };
+
   return(
     <>
       <div className='homeCategoryHeader'>Discography</div>
+      <ToggleButtonGroup
+        exclusive
+        value={albumState}
+        onChange={handleAlbumChange}
+      >
+        <ToggleButton value={albumStates.All}>{albumStates.All}</ToggleButton>
+        <ToggleButton value={albumStates.Albums}>{albumStates.Albums}</ToggleButton>
+        <ToggleButton value={albumStates.Singles}>{albumStates.Singles}</ToggleButton>
+        <ToggleButton value={albumStates.Others}>{albumStates.Others}</ToggleButton>
+      </ToggleButtonGroup>
       <Stack className='cardContainer'>
         
       </Stack>
