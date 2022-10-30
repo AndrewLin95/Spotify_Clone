574d78bec003bd04bfbf5e01081fecadb223bcbc
AndrewLin95 <andrew.lin88@hotmail.com>
2022-10-30 14:02:09 -0400

    added left side bar home and search

--- src/Components/LeftSideBar/LeftSideBar.tsx
+++ src/Components/LeftSideBar/LeftSideBar.tsx
@@ -1,5 +1,8 @@
 import { FC } from 'react';
 import { Link } from 'react-router-dom';
+import { faHouse, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
 import './style.css';
 
 interface Props{
@@ -10,6 +13,15 @@ interface Props{
 const LeftSideBar: FC<Props> = ({dataPlaylist, handlePlaylistClick}) => {
   return (
     <div id="LeftSideBarMainContainer">
+      <div id='leftSideBarTopContainer'>
+        <div>
+          <Link className='homeBtn' to="/home" style={{textDecoration: 'none'}}><FontAwesomeIcon icon={faHouse}/> Home </Link> 
+        </div>
+        <div>
+          {/* to add link to search page */}
+          <FontAwesomeIcon id='headerSearchIcon' icon={faMagnifyingGlass} /> Search
+        </div>
+      </div>
       <div id='leftSideBarSeperator'></div>
       <div id='leftSideBarPlaylistContainer'>
         {Object.entries(dataPlaylist).map(([key, value]) => {
