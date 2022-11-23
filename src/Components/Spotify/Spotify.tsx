d91166183f45060aa1b82838366288f347bd34d6
AndrewLin95 <andrew.lin88@hotmail.com>
2022-11-22 20:39:29 -0500

    switch to prod

--- src/Components/Spotify/Spotify.tsx
+++ src/Components/Spotify/Spotify.tsx
@@ -1,32 +1,33 @@
 // Spotify API types
 // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/spotify-api/index.d.ts
 
-import SpotifyWebApi from "spotify-web-api-js";
+import SpotifyWebApi from 'spotify-web-api-js';
 
-export const authEndpoint = "https://accounts.spotify.com/authorize";
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
 // for development
-const redirectUri = "http://localhost:3000";
+// const redirectUri = "http://localhost:3000";
 // for production
-// const redirectUri = "https://andrewlin95.github.io/spotify_clone/"
-const clientId = "44531b7506a649129f802b598710dfab";
+const redirectUri =
+  'http://al.spotify-clone.s3-website.us-east-2.amazonaws.com/';
+const clientId = '44531b7506a649129f802b598710dfab';
 
 const scopes = [
-  "user-read-currently-playing",
-  "user-read-recently-played",
-  "user-read-playback-state",
-  "user-top-read",
-  "streaming",
-  "user-modify-playback-state",
-  "user-read-private",
-  "user-read-email"
+  'user-read-currently-playing',
+  'user-read-recently-played',
+  'user-read-playback-state',
+  'user-top-read',
+  'streaming',
+  'user-modify-playback-state',
+  'user-read-private',
+  'user-read-email'
 ];
 
 export const getTokenFromUrl = () => {
   return window.location.hash
     .substring(1)
-    .split("&")
+    .split('&')
     .reduce((initial: any, item) => {
-      let parts = item.split("=");
+      let parts = item.split('=');
       initial[parts[0]] = decodeURIComponent(parts[1]);
       return initial;
     }, {});
@@ -35,5 +36,5 @@ export const getTokenFromUrl = () => {
 export const spotifyAPI = new SpotifyWebApi();
 
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-  "%20"
+  '%20'
 )}&response_type=token&show_dialog=true`;
